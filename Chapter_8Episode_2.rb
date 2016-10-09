b0f9160f7f6f1c3dc1b663510fe2b3597fccef33
Yuanhao Sun <sunyuanhao123456@gmail.com>
2016-10-09 14:46:51 +0800

    Daily ruby files

--- Chapter_8Episode_2.rb
+++ Chapter_8Episode_2.rb
@@ -1,9 +1,31 @@
 # encoding: utf-8
-# 类方法
+# 类方法 类变量
 
-class << Helloworld
-	def hello(name)
-		p "#{name} said hello."
+class Hellocount 
+	@@count = 0 #用@@开头的变量称之为类变量
+
+	def Hellocount.count #这个方法被称作类方法
+		@@count
+	end
+
+	class << Hellocount #类方法的另一种写法 Hellocount也可以换成self
+		def count_ano
+			@@count
+		end
+	end
+
+	def initialize(myname="ruby")
+		@name = myname
+	end
+
+	def hello
+		@@count += 1
+		p "Hello, world. I'm #{@name}"
 	end
 end
-Helloworld.hello("Ruby")
+
+Xu = Hellocount.new("Zephyr")
+p Hellocount.count
+Xu.hello
+Xu.hello
+p Hellocount.count
