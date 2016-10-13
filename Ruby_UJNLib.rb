addca5ffbc4d19ef5fd33a57abb07cbef5853fc0
Yuanhao Sun <sunyuanhao123456@gmail.com>
2016-10-13 17:22:13 +0800

    UJN library ruby script

--- Ruby_UJNLib.rb
+++ Ruby_UJNLib.rb
@@ -13,7 +13,39 @@ def getToken
     url = URI(temp)
     json = Net::HTTP.get(url)
     obj = JSON.parse(json)
-    p obj["status"]
+    if obj["status"] == "success"
+        return obj["data"]["token"]
+    else
+        p "Connection wrong!"
+    end
 end
 
-pp getToken()
+def checkToken
+    temp = "http://seat.ujn.edu.cn/rest/v2/user/reservations?token=#{getToken}"
+    url = URI(temp)
+    json = Net::HTTP.get(url)
+    obj = JSON.parse(json)
+    if obj["status"] == "success"
+        return true
+    else
+        return false
+    end
+end
+
+def getBuildingsInfo
+    temp = "http://seat.ujn.edu.cn/rest/v2/room/stats2/2?token=#{getToken}"
+    url = URI(temp)
+    json = Net::HTTP.get(url)
+    obj = JSON.parse(json)
+    if obj["status"] == "success"
+        obj["data"].each do |room|
+            p "ID:#{room["roomId"]} #{room["room"]} 楼层:#{room["floor"]} 剩余:#{room["free"]}"
+        end
+    else
+        p "Failed to get information from client"
+    end
+end
+
+def getSeatInfo
+    begintimeurl
+end
