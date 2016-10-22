12d0759ef5a4b67923d7b573f1c8d742e2ac9f84
Yuanhao Sun <sunyuanhao123456@gmail.com>
2016-10-22 12:00:09 +0800

    0.jpg can be used by this script

--- Get_Pic.rb
+++ Get_Pic.rb
@@ -5,9 +5,6 @@ require 'open-uri'
 base_url = 'http://7xqh4i.com1.z0.glb.clouddn.com/pic'
 
 331.times do |i|
-    if i == 0
-        next
-    end
     fn = "#{i.to_s}.jpg"
     url = base_url + "#{i.to_s}.jpg"
     open(url) do |io|
