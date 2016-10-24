2cb03240239274ef81d7afc08b8b007cf280b6cd
Yuanhao Sun <sunyuanhao123456@gmail.com>
2016-10-24 20:36:28 +0800

    Daily ruby files

--- telegram-bot.rb
+++ telegram-bot.rb
@@ -33,6 +33,13 @@ Telegram::Bot::Client.run(token) do |bot|
             #bot.api.send_message(chat_id: message.chat.id, text: "#{line}")
             bot.api.send_chat_action(chat_id: message.chat.id, action: "upload_photo")
             bot.api.send_photo(chat_id: message.chat.id, photo: "#{url}", caption: "#{line}")
+        when '/randomstu'
+            line = randomnum()
+            fn = "#{line.to_s}.jpg"
+            url = base_url + "#{line[1..4]}/" + fn[1..15]
+            id = message.chat.id.to_s
+            bot.api.send_chat_action(chat_id: id, action: "upload_photo")
+            bot.api.send_photo(chat_id: id, photo: "#{url}", caption: "#{line}")
         end
     end
 end
