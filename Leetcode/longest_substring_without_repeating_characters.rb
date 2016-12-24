44f5ab3aa6905a83ee80d5a1488efa96e0a4aade
Yuanhao Sun <sunyuanhao123456@gmail.com>
2016-12-25 00:14:19 +0800

    12-24

--- Leetcode/longest_substring_without_repeating_characters.rb
+++ Leetcode/longest_substring_without_repeating_characters.rb
@@ -13,4 +13,7 @@ def length_of_longest_substring(s)
         substring << ch
     end
     maxlen = substring.length if maxlen < substring.length
+    # 存在特殊情况 如果重复ch的话会导致substring里面的元素减少
+
+    maxlen
 end
