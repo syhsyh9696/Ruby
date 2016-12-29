a024306a6dc965215845d60671b8510ba95e83b5
Yuanhao Sun <sunyuanhao123456@gmail.com>
2016-12-29 20:36:34 +0800

    modify

--- Leetcode/longest_palindromic_substring.rb
+++ Leetcode/longest_palindromic_substring.rb
@@ -3,17 +3,27 @@
 # @return {String}
 
 def longest_palindrome(s)
-    substr, maxlen  = '', 0
-    
-    s.each_char do |ch|
-        if substr.include?(ch)
-            substr = substr[substr.index(ch)..-1] + ch
-            maxlen = substr.length if maxlen < substr.length
-        else
-            substr << ch
+    # return s if s.size < 2
+    # return s[0] == s[1] ? s : s[0] if s.size == 2
+
+    middle, offset, maxstr = (0 + s.size) / 2, 0, ''
+
+    while true
+        return maxstr if (middle - offset) < 0 && (middle + offset) >= s.size
+        return maxstr if (middle - offset + 1) * 2 < maxstr.size
+
+        [-1, 1].each do |sign|
+            idx = middle + sign * offset
+            str = _longest_palindrome_(s, idx, idx)
+            maxstr = str if maxstr.size < str.size
+
+            if s[idx] == s[idx + 1]
+                str = _long_palindrome_(s, idx, idx + 1)
+                maxstr = str if maxstr.size < str.size
+            end
+
+            
         end
-
+        
     end
-
-    substr
 end
