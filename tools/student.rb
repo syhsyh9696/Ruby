f77a27bb56016da40261183b283de79918a3febf
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-06-19 10:20:03 +0800

    Fix a bug

--- tools/student.rb
+++ tools/student.rb
@@ -8,7 +8,7 @@ require 'pp'
 def client()
     client = Mysql2::Client.new(:host => "127.0.0.1",
                                 :username => "root",
-                                :password => "student",
+                                :password => "default",
                                 :database => "student_dl")
 end
 
@@ -27,14 +27,17 @@ end
 def get_face_mark url
     client = Mechanize.new
     client.user_agent = Mechanize::AGENT_ALIASES.values[rand(21)]
-    # json = get_face_url(stu_id); value = Hash.new
-
-    value = "MsgId=" + Time.new.to_i.to_s + "063" + "&CreateTime=" + Time.new.to_i.to_s + "&Content%5BimageUrl%5D="+ "https://mediaplatform.msxiaobing.com" + url
+    random_num = (100 + rand(80)).to_s
+    
+    value = "MsgId=" + Time.new.to_i.to_s + random_num +
+            "&CreateTime=" + Time.new.to_i.to_s +
+            "&Content%5BimageUrl%5D="+ "https://mediaplatform.msxiaobing.com" + url
 
     result = client.post "http://kan.msxiaobing.com/Api/ImageAnalyze/Process?service=yanzhi",
                          value, ({'Content-Type' => 'application/x-www-form-urlencoded'})
 
     mark = JSON.parse(result.body)
+    return nil if mark == nil
 
     mark['content']['text'].encode("UTF-8")
 end
@@ -68,7 +71,13 @@ def ms_mark
         # temp.class = Json
         next if item == nil
         mark_temp = get_face_mark(item['ms_url']).to_s
-        # mark_temp = temp['content']['text'].to_s
+        dot = mark_temp.index('.')
+        next if dot == nil
+        
+        mark = 10.to_f if mark_temp[dot - 1] == '0'
+        mark = "#{mark_temp[dot - 1]}.#{mark_temp[dot + 1]}".to_f if mark_temp[dot - 1] != '0'
+        client.query("update info_old set ms_mark=#{mark} where stu_num=\'#{item['stu_num']}\'")
+=begin        
         if mark_temp =~ /\d\d.\d/
             client.query("update info_old set ms_mark=10.0 where stu_num=\'#{item['stu_num']}\'")
         elsif mark_temp =~ /\d.\d/
@@ -77,6 +86,7 @@ def ms_mark
         else
             next
         end
+=end
     end
     client.close
 end
