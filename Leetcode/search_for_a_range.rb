8600c1375c75c1b62d330d21fe409c17bba5c7ca
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-10-11 14:29:53 +0800

    2017-10-11

--- Leetcode/search_for_a_range.rb
+++ Leetcode/search_for_a_range.rb
@@ -3,11 +3,20 @@
 # @param {Integer} target
 # @return {Integer[]}
 
-def search_range(nums, target)
-    lbound = (0...nums.size).bsearch{ |i| nums[i] >= target }
-    return [-1, 1] if lbound.nil?
-    return [-1, 1] if lbound != target
 
-    return [lbound, lbound + 1]
+def search_range(nums, target)
+    lbound = nums.bsearch{ |i| i >= target }
+    return [-1, -1] if lbound.nil?
+    return [-1, -1] if lbound != target
     
+    rbound = Hash[nums.map.with_index.to_a][target]
+    return [nums.index(lbound), rbound] if rbound != nil
+    return [nums.index(lbound), nums.index(lbound)]
+end
+
+
+require 'pp'
+def search_range(nums, target)
+    hash = 
+    pp hash
 end
